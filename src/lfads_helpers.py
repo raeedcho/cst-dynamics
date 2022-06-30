269e75f1ea810d0947f72272eb2d515045fc8d65
Raeed Chowdhury <raeed.chowdhury@gmail.com>
2022-06-30 18:31:15 -0400

    refactor: Move lfads prep to its own function

--- src/lfads_helpers.py
+++ src/lfads_helpers.py
@@ -3,8 +3,10 @@ import pyaldata
 import h5py
 import os
 from sklearn.model_selection import train_test_split
+from sklearn.decomposition import PCA
 
-from lfads_tf2.utils import chop_data, merge_chops
+from lfads_tf2.utils import chop_data, merge_chops, load_data, load_posterior_averages
+from . import data
 
 
 def prep_neural_tensors(
@@ -142,3 +144,51 @@ def add_lfads_rates(
 
     return trial_data
 
+@pyaldata.copy_td
+def prep_data_with_lfads(td, task, lfads_params):
+    td_task = td.groupby('task').get_group(task).copy()
+
+    td_task = pyaldata.combine_time_bins(td_task,int(lfads_params['bin_size']/td_task['bin_size'].values[0]))
+
+    trial_ids = load_data(
+        '../data/pre-lfads/', 
+        prefix=f'Earl_20190716_{task}', 
+        signal='trial_id',
+        merge_tv=True
+    )[0].astype(int)
+
+    post_data = load_posterior_averages(f'../results/autolfads/Earl_20190716/autolfads_with_jitter/{task.lower()}', merge_tv=True)
+    td_task = add_lfads_rates(
+        td_task,
+        post_data.rates/lfads_params['bin_size'],
+        chopped_trial_ids=trial_ids,
+        overlap=lfads_params['overlap'],
+        new_sig_name='lfads_rates',
+    )
+    td_task = add_lfads_rates(
+        td_task,
+        post_data.gen_inputs/lfads_params['bin_size'],
+        chopped_trial_ids=trial_ids,
+        overlap=lfads_params['overlap'],
+        new_sig_name='lfads_inputs',
+    )
+
+    td_task = data.trim_nans(td_task,ref_signals=['rel_hand_pos','lfads_rates'])
+    td_task = data.fill_kinematic_signals(td_task)
+
+    td_task['M1_rates'] = [
+        pyaldata.smooth_data(
+            spikes/bin_size,
+            dt=bin_size,
+            std=0.05,
+            backend='convolve',
+        ) for spikes,bin_size in zip(td_task['M1_spikes'],td_task['bin_size'])
+    ]
+
+    M1_pca_model = PCA()
+    td_task = pyaldata.dim_reduce(td_task,M1_pca_model,'M1_rates','M1_pca')
+    
+    lfads_pca_model = PCA()
+    td_task = pyaldata.dim_reduce(td_task,lfads_pca_model,'lfads_rates','lfads_pca')
+
+    return td_task
