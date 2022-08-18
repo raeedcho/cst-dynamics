0e9713cf5cfaeec3a68f6985c773b86b5a59cf6f
Raeed Chowdhury <raeed.chowdhury@gmail.com>
2022-08-18 18:06:03 -0400

    feat: add mean subtraction to normalized cross-corelation (to match with Pearson's r calculation)

--- src/analysis.py
+++ src/analysis.py
@@ -9,7 +9,10 @@ def normalized_cross_correlation(x, y, **kwargs):
     '''
     Calculate the normalized cross-correlation between two signals, using scipy.signal.correlate.
     This function normalizes the correlation at each lag by the number of points within the valid
-    valid window (i.e. it masks out the zero-padded portion to get the normalization)
+    valid window (i.e. it masks out the zero-padded portion to get the normalization). The normalization
+    factor is the product of the root mean squares of the input signals (which is the same as the 
+    auto-correlation at lag 0). Also, we want to mean subtract the two signals before calculating
+    the correlation to make things match up with a Pearson's r type of intution.
     
     Args:
         x (array): first signal
@@ -19,11 +22,13 @@ def normalized_cross_correlation(x, y, **kwargs):
     Returns:
         array: normalized cross-correlation
     '''
+    x_hat = x-np.mean(x)
+    y_hat = y-np.mean(y)
     
     norm_factor = (
         signal.correlate(np.ones(x.shape), np.ones(y.shape), **kwargs)
         *
-        np.sqrt(np.mean(x**2) * np.mean(y**2))
+        np.sqrt(np.mean(x_hat**2) * np.mean(y_hat**2))
     )
 
-    return signal.correlate(x, y, **kwargs) / norm_factor
+    return signal.correlate(x_hat, y_hat, **kwargs) / norm_factor
