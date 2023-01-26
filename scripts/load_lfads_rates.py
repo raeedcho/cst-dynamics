443f7f3dfa7109667c8858c2ff5e71a95fac85ac
Raeed Chowdhury <raeed.chowdhury@gmail.com>
2023-01-26 17:25:02 -0500

    feat: Add some simple plotting to CST/RTT lfads exploration script

--- scripts/load_lfads_rates.py
+++ scripts/load_lfads_rates.py
@@ -3,7 +3,9 @@ from lfads_tf2.utils import load_data, load_posterior_averages
 import src
 import pyaldata
 import yaml
+
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 with open("../params.yaml", "r") as params_file:
     params = yaml.safe_load(params_file)["lfads_prep"]
@@ -18,4 +20,13 @@ load_params = {
 }
 trial_data = src.data.load_clean_data(**load_params)
 
-#%%
+# %%
+trial_data['trialtime'] = trial_data['Time from go cue (s)']
+
+trialnum = 15
+fig,ax = plt.subplots(3,1)
+src.plot.plot_hand_trace(trial_data.loc[trialnum],ax=ax[0])
+ax[1].plot(trial_data.loc[trialnum]['lfads_rates'][:,0:15])
+ax[2].plot(trial_data.loc[trialnum]['lfads_inputs'])
+
+# %%
