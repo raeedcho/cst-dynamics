a021c42d5786b79d7111d20978c41ee98d619e64
Raeed Chowdhury <raeed.chowdhury@gmail.com>
2023-01-27 17:54:27 -0500

    feat: Add some code to look at LFADS rates during CST/RTT

--- scripts/load_lfads_rates.py
+++ scripts/load_lfads_rates.py
@@ -1,8 +1,13 @@
 #%%
+import numpy as np
+
 from lfads_tf2.utils import load_data, load_posterior_averages
 import src
 import pyaldata
 import yaml
+
+from sklearn.decomposition import PCA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -18,15 +23,130 @@ load_params = {
     'bin_size': 0.01,
     'lfads_params': params,
 }
-trial_data = src.data.load_clean_data(**load_params)
+td = (
+    src.data.load_clean_data(**load_params)
+    .assign(**{'trialtime': lambda x: x['Time from go cue (s)']})
+    .pipe(pyaldata.dim_reduce,PCA(n_components=15),'MC_rates','MC_pca')
+    .pipe(pyaldata.dim_reduce,PCA(n_components=15),'lfads_rates','lfads_pca')
+)
 
 # %%
-trial_data['trialtime'] = trial_data['Time from go cue (s)']
+trialnum = 228
+trial = td.loc[td['trial_id']==trialnum].squeeze()
+fig,ax = plt.subplots(4,1,figsize=(10,10))
+src.plot.plot_hand_trace(trial,ax=ax[0])
+src.plot.plot_hand_velocity(trial,ax=ax[1])
+ax[2].plot(trial['lfads_pca'][:,0:3])
+ax[3].plot(trial['lfads_inputs'])
 
-trialnum = 15
-fig,ax = plt.subplots(3,1)
-src.plot.plot_hand_trace(trial_data.loc[trialnum],ax=ax[0])
-ax[1].plot(trial_data.loc[trialnum]['lfads_rates'][:,0:15])
-ax[2].plot(trial_data.loc[trialnum]['lfads_inputs'])
+# %%
+fig,ax = plt.subplots(1,1)
+task_colors = {'CST': 'C0','RTT': 'C1'}
+signal = 'lfads_pca'
+trials_to_plot = [227,228]
+for trial_id in trials_to_plot:
+    trial = td.loc[td['trial_id']==trial_id].squeeze()
+    ax.plot(
+        trial[signal][:,0],
+        trial[signal][:,1],
+        color=task_colors[trial['task']],
+    )
 
+sns.despine(ax=ax,left=True,bottom=True)
+
+#%% Context space
+
+signal = 'lfads_inputs'
+tonic_context_model = LinearDiscriminantAnalysis()
+td_models = src.data.rebin_data(td,new_bin_size=0.100)
+tonic_context_model.fit(
+    np.row_stack(td_models.apply(lambda x: x[signal][x['idx_goCueTime']+20,:],axis=1)),
+    td_models['task'],
+)
+
+transient_context_model = LinearDiscriminantAnalysis()
+transient_context_model.fit(
+    np.row_stack(td_models.apply(lambda x: x[signal][x['idx_pretaskHoldTime']+3,:],axis=1)),
+    td_models['task'],
+)
+
+def norm_vec(vec):
+    return vec/np.linalg.norm(vec)
+
+td['Motor Cortex Tonic Context Dim'] = [(sig @ norm_vec(tonic_context_model.coef_).squeeze()[:,None]).squeeze() for sig in td[signal]]
+td['Motor Cortex Transient Context Dim'] = [(sig @ norm_vec(transient_context_model.coef_).squeeze()[:,None]).squeeze() for sig in td[signal]]
+
+td_explode = (
+    td
+    .assign(
+        **{'Hand velocity (cm/s)': lambda x: x.apply(lambda y: y['hand_vel'][:,0],axis=1)}
+    )
+    .filter(items=[
+        'trial_id',
+        'Time from go cue (s)',
+        'Time from task cue (s)',
+        'task',
+        'Motor Cortex Transient Context Dim',
+        'Motor Cortex Tonic Context Dim',
+        'Hand velocity (cm/s)'
+    ])
+    .explode([
+        'Time from go cue (s)',
+        'Time from task cue (s)',
+        'Motor Cortex Transient Context Dim',
+        'Motor Cortex Tonic Context Dim',
+        'Hand velocity (cm/s)',
+    ])
+    .astype({
+        'Time from go cue (s)': float,
+        'Time from task cue (s)': float,
+        'Motor Cortex Transient Context Dim': float,
+        'Motor Cortex Tonic Context Dim': float,
+        'Hand velocity (cm/s)': float,
+    })
+    # .loc[lambda df: df['Time from go cue (s)']>0]
+    # .loc[lambda df: (df['Time from go cue (s)']<0) & (df['Time from go cue (s)']>-0.5)]
+)
+avg_trial = td_explode.groupby(['Time from go cue (s)','task']).mean().loc[-1:5].reset_index()
+task_colors={'RTT': 'C1','CST': 'C0'}
+fig,axs = plt.subplots(2,1,sharex=True,figsize=(6,6))
+epoch = 'task'
+for _,trial in td.groupby('task').sample(n=10).iterrows():
+    # put an average trace over this thing
+    axs[0].plot(
+        trial[f'Time from {epoch} cue (s)'],
+        trial['Motor Cortex Transient Context Dim'],
+        color=task_colors[trial['task']],
+        alpha=0.3,
+        lw=2,
+    )
+    axs[1].plot(
+        trial[f'Time from {epoch} cue (s)'],
+        trial['Motor Cortex Tonic Context Dim'],
+        color=task_colors[trial['task']],
+        alpha=0.3,
+        lw=2,
+    )
+    # axs.set_xlim([-1,5])
+    # axs.set_ylim([-0.3,0.3])
+    # axs.set_ylabel(f'Comp {compnum+1}')
+for task,trial in avg_trial.groupby('task'):
+    axs[0].plot(
+        trial[f'Time from {epoch} cue (s)'],
+        trial['Motor Cortex Transient Context Dim'],
+        color=task_colors[task],
+        lw=4,
+    )
+    axs[1].plot(
+        trial[f'Time from {epoch} cue (s)'],
+        trial['Motor Cortex Tonic Context Dim'],
+        color=task_colors[task],
+        lw=4,
+    )
+axs[0].set_ylabel('Motor Cortex\nContext Dim')
+axs[1].set_ylabel('Motor Cortex\nContext Dim')
+#axs[2].set_ylabel('Behavioral\nContext Dim')
+axs[-1].set_xlabel(f'Time from {epoch} cue (s)')
+axs[0].set_xlim([-1,5])
+sns.despine(fig=fig,trim=True)
 # %%
