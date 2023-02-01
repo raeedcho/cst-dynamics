8259a8dd00f073693a5c52fcefa25d4d92c9ee7f
Raeed Chowdhury <raeed.chowdhury@gmail.com>
2023-02-01 17:47:02 -0500

    feat: Add decoding analysis for LFADS rates

--- scripts/load_lfads_rates.py
+++ scripts/load_lfads_rates.py
@@ -1,13 +1,15 @@
 #%%
 import numpy as np
 
-from lfads_tf2.utils import load_data, load_posterior_averages
 import src
 import pyaldata
 import yaml
+import pandas as pd
 
-from sklearn.decomposition import PCA
+from sklearn.decomposition import PCA, TruncatedSVD
+from sklearn.linear_model import LinearRegression
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.model_selection import GroupShuffleSplit
 from src.models import SSA
 
 import seaborn as sns
@@ -211,5 +213,130 @@ for colnum,trial_id in enumerate(trials_to_plot):
     trial = td.loc[td['trial_id']==trial_id].squeeze()
     plot_trial(trial,axs[:,colnum],signal_to_plot='lfads_ssa')
 
+
+# %% decoding
+
+signal = 'lfads_pca'
+def get_test_labels(df):
+    gss = GroupShuffleSplit(n_splits=1,test_size=0.25)
+    _,test = next(gss.split(
+        df['lfads_pca'],
+        df['True velocity'],
+        groups=df['trial_id'],
+    ))
+    return np.isin(np.arange(df.shape[0]),test)
+
+def fit_models(df):
+    # individual models
+    models = {}
+    for task in df['task'].unique():
+        models[task] = LinearRegression()
+        train_df = df.loc[(~df['Test set']) & (df['task']==task)]
+        models[task].fit(
+            np.row_stack(train_df['lfads_pca']),
+            train_df['True velocity'],
+        )
+
+    # joint models
+    models['Joint'] = LinearRegression()
+    train_df = df.loc[~df['Test set']]
+    models['Joint'].fit(
+        np.row_stack(train_df['lfads_pca']),
+        train_df['True velocity'],
+    )
+
+    return models
+
+def model_predict(df,models):
+    ret_df = df.copy()
+    for model_name,model in models.items():
+        ret_df = ret_df.assign(**{
+            f'{model_name} predicted': model.predict(np.row_stack(ret_df['lfads_pca']))
+        })
+    return ret_df
+
+def score_models(df,models):
+    scores = pd.Series(index=pd.MultiIndex.from_product(
+        [df['task'].unique(),models.keys()],
+        names=['Test data','Train data']
+    ))
+    for task in df['task'].unique():
+        for model_name, model in models.items():
+            test_df = df.loc[df['Test set'] & (df['task']==task)]
+            scores[(task,model_name)] = model.score(np.row_stack(test_df['lfads_pca']),test_df['True velocity'])
+    
+    return scores
+
+td_train_test = (
+    td
+    .assign(
+        **{'True velocity': lambda df: df.apply(lambda s: s['hand_vel'][:,0],axis=1)}
+    )
+    .filter(items=[
+        'trial_id',
+        'Time from go cue (s)',
+        'task',
+        'True velocity',
+        'lfads_pca',
+    ])
+    .explode([
+        'Time from go cue (s)',
+        'True velocity',
+        'lfads_pca',
+    ])
+    .astype({
+        'Time from go cue (s)': float,
+        'True velocity': float,
+    })
+    .assign(**{'Test set': lambda df: get_test_labels(df)})
+)
+
+models = fit_models(td_train_test)
+scores = score_models(td_train_test,models)
+td_pred = (
+    td_train_test
+    .pipe(model_predict,models)
+    .melt(
+        id_vars=['trial_id','Time from go cue (s)','task'],
+        value_vars=['True velocity','CST predicted','RTT predicted','Joint predicted'],
+        var_name='Model',
+        value_name='Hand velocity (cm/s)',
+    )
+)
+
+# trials_to_plot=[71,52]
+trials_to_plot=td_pred.groupby('task').sample(n=1)['trial_id']
+g=sns.relplot(
+    data=td_pred.loc[np.isin(td_pred['trial_id'],trials_to_plot)],
+    x='Time from go cue (s)',
+    y='Hand velocity (cm/s)',
+    hue='Model',
+    hue_order=['True velocity','CST predicted','RTT predicted','Joint predicted'],
+    palette=['k','C0','C1','0.5'],
+    kind='line',
+    row='trial_id',
+    row_order=trials_to_plot,
+    height=4,
+    aspect=2,
+)
+g.axes[0,0].set_yticks([-200,0,200])
+g.axes[0,0].set_xticks([0,2,4,6])
+sns.despine(fig=g.fig,trim=True)
+
+# fig_name = src.util.format_outfile_name(td,postfix='cst71_rtt52_vel_pred')
+# g.fig.savefig(os.path.join('../results/2022_sfn_poster/',fig_name+'.pdf'))
+
+fig,ax = plt.subplots(1,1)
+sns.heatmap(
+    ax=ax,
+    data=scores.unstack(),
+    vmin=0,
+    vmax=1,
+    annot=True,
+    annot_kws={'fontsize': 21},
+    cmap='gray',
+)
+# fig_name = src.util.format_outfile_name(td,postfix='cst_rtt_vel_pred_scores')
+# fig.savefig(os.path.join('../results/2022_sfn_poster/',fig_name+'.pdf'))
 
 # %%
