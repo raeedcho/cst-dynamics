398849857ef2ad2811d3b33d45e2e780c98b0613
Raeed Chowdhury <raeed.chowdhury@gmail.com>
2024-02-08 16:38:36 -0500

    docs: add description of dekodec

--- src/dekodec.py
+++ src/dekodec.py
@@ -1,3 +1,45 @@
+'''
+DekODec (Dekleva Orthonormal Decomposition) is based on the manifold 
+view of neural activity, in which we can represent population activity 
+as a point (state) within a low dimensional manifold. For different 
+tasks, the neural state may inhabit different subspaces within the 
+manifold. If the neural state projects in part onto an axis or subspace 
+*only* during condition A, then we consider that axis/subspace to be
+"A-unique". Likewise, if the neural state contains projections onto a 
+different axis/subspace during multiple conditions (e.g. A, B, and C),
+we consider it to be "ABC-shared".
+
+The intuition behind the present method is as follows: 
+Consider three conditions, A, B and C. If there is some axis/subspace
+that is unique to A, then it must exist in the null space of the
+space defeined by B and C. Likewise, a B-unique subspace exists in the
+null space of A and C, etc. We use PCA and a variance cutoff to 
+identify the relevant null spaces for identifying unique activity: 
+
+    A_unique exists in {B,C}_null, B_unique in {A,C}_null, and C_unique
+    in {A,B} null
+
+Doing so gives us the **form** of the unique activity, but since each
+condition is calculated separately, they will not be mathematically
+orthogonal (each basis estimation will incorporate some degree of 
+noise, largely due to low-variance dimensions in the original data). 
+
+However, once we have identified the profiles of the unique activity,
+we can find an orthonormal transformation of the original space that
+reconstructs those known profiles. We do this by minimizing sum 
+squared error using the Manopt toolbox. The output of the optimization
+reflects the combination of unique spaces, and we can simply take the
+null space of this to define the shared space. 
+
+***The end result is an
+orthonormal transformation of the original basis such that each axis
+reflects either condition-unique or condition-shared activity***
+
+
+----
+created by Brian Dekleva 5/2/2023
+adapted for python by Raeed Chowdhury 7/19/2023
+'''
 import numpy as np
 from scipy.linalg import null_space
 import torch
